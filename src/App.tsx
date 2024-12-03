3fba507336735c5b7c9186197b5aaea10071ea56
niceman114 <niceman114@gmail.com>
2024-12-03 11:48:27 +0900

    ga

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import React, { useEffect, useState } from 'react';
 import './App.css';
+import GA from './GA';
 
 function App() {
   const [ip, setIp] = useState('?');
   const [isVisible, setIsVisible] = useState(false);
+  const ga = GA('Visited');
 
   useEffect(() => {
       (async () => {
@@ -25,9 +27,9 @@ function App() {
         <header className="App-header">
           <h1>My IP address is</h1>
         </header>
-        <main>
-          {ip ? <p className="ip-address">{ip}</p> : <p className="ip-address loading">Loading...</p>}
-          {isVisible && <p className="error hide">Sorry, failed to find IP information.</p>}
+        <main onMouseOver={() => ga('mouse over')}>
+          {ip ? <p className="ip-address" onClick={() => ga('ip address clicked')}>{ip}</p> : <p className="ip-address loading">Loading...</p>}
+          {isVisible && <p className="error hide">Sorry, failed to find IP information.</p> }
         </main>
       </div>
   );
