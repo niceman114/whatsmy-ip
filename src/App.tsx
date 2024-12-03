f157b85afc01d4b7af1a402444c3e27d63b43d2c
niceman114 <niceman114@gmail.com>
2024-12-03 12:35:09 +0900

    test

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,19 @@
 import React, { useEffect, useState } from 'react';
 import './App.css';
-import ReactGA from 'react-ga';
+import ReactGA from 'react-ga4';
 import GA from './GA';
 
 const TRACKING_ID = 'G-YLXZK5F931';
 ReactGA.initialize(TRACKING_ID);
 
 function App() {
-  ReactGA.pageview(window.location.pathname);
+  ReactGA.send({
+    hitType: 'pageview1',
+    page: window.location.pathname
+  });
 
   const [ip, setIp] = useState('?');
   const [isVisible, setIsVisible] = useState(false);
-  const eventTracker = GA('Visited');
 
   useEffect(() => {
       (async () => {
@@ -33,8 +35,8 @@ function App() {
         <header className="App-header">
           <h1>My IP address is</h1>
         </header>
-        <main onMouseOver={() => eventTracker('mouse over')}>
-          {ip ? <p className="ip-address" onClick={() => eventTracker('ip address clicked')}>{ip}</p> : <p className="ip-address loading">Loading...</p>}
+        <main>
+          {ip ? <p className="ip-address">{ip}</p> : <p className="ip-address loading">Loading...</p>}
           {isVisible && <p className="error hide">Sorry, failed to find IP information.</p> }
         </main>
       </div>
